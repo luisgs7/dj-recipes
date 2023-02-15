212f05176250b6046587a90de74a3cd0caa4d01a
luisgs7 <luisgsilva270@gmail.com>
2023-02-15 10:40:00 -0300

    remove views and urls, contato and sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path, include
-from recipes.views import home, contato, sobre
+from recipes.views import home
 
 
 urlpatterns = [
     path("", home),
-    path("sobre/", sobre),
-    path("contato/", contato),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -3,10 +3,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,"recipes/home.html")
-
-def sobre(request):
-    return HttpResponse("Sobre") 
-
-def contato(request):
-    return render(request, "recipes/contato.html")
-
