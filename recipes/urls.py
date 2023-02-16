a7b5ecbb1c6205e007d63d663f43fcfa5635f4a5
luisgs7 <luisgsilva270@gmail.com>
2023-02-16 12:07:07 -0300

    update tag a

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 from recipes import views
 
+app_name = "recipes"
 
 urlpatterns = [
-    path("", views.home),
-    path("recipes/<int:id>/", views.recipe)
+    path("", views.home, name="home"),
+    path("recipes/<int:id>/", views.recipe, name="recipe")
 ]
