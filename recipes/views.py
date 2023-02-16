d095acd7a449468cfba98c2acaf3dfd908df6051
luisgs7 <luisgsilva270@gmail.com>
2023-02-16 10:24:45 -0300

    new page recipe-view

--- recipes/views.py
+++ recipes/views.py
@@ -8,7 +8,7 @@ def home(request):
                   })
 
 def recipe(request, id):
-    return render(request, "recipes/pages/home.html",
+    return render(request, "recipes/pages/recipe-view.html",
                   context={
                     "name": "Recipe#01"
                   })
