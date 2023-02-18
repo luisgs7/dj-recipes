8f440ed7e36f588fb7a3203f3e1802341711a690
luisgs7 <luisgsilva270@gmail.com>
2023-02-18 16:11:03 -0300

    corrigindo erros de valores None

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,5 +5,6 @@ app_name = "recipes"
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("recipes/<int:id>/", views.recipe, name="recipe")
+    path("recipes/<int:id>/", views.recipe, name="recipe"),
+    path("recipes/category/<int:category_id>/", views.category, name="category"),
 ]
