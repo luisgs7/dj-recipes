69d375ddfefb38a96d1b4f297d99f71459de339e
luisgs7 <luisgsilva270@gmail.com>
2023-02-19 15:02:43 -0300

    format code

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -8,10 +8,6 @@ class RecipeViewsTest(TestCase):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
 
-    def test_recipe_category_view_function_is_correct(self):
-        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
-        self.assertIs(view.func, views.category)
-
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
@@ -29,3 +25,7 @@ class RecipeViewsTest(TestCase):
         self.assertIn('<h1>No recipes found here :(</h1>',
                       response.content.decode('utf-8'))
         # A função decode é para converter de bytes para string
+
+    def test_recipe_category_view_function_is_correct(self):
+        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
+        self.assertIs(view.func, views.category)
