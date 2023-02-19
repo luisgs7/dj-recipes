9001e99827bb81e3b963f6514dc6bf11a237b6c6
luisgs7 <luisgsilva270@gmail.com>
2023-02-19 16:05:28 -0300

    new test: test_recipe_home_template_loads_recipes

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
+from recipes.models import Category, Recipe
+from django.contrib.auth.models import User
 
 
 class RecipeViewsTest(TestCase):
@@ -21,6 +23,31 @@ class RecipeViewsTest(TestCase):
         self.assertIn('<h1>No recipes found here :(</h1>',
                       response.content.decode('utf-8'))
         # A função decode é para converter de bytes para string
+
+    def test_recipe_home_template_loads_recipes(self):
+        category = Category.objects.create(name='Category')
+        author = User.objects.create_user(
+            first_name='user',
+            last_name='name',
+            username='username',
+            password='123456',
+            email='username@email.com',
+        )
+        recipe = Recipe.objects.create(
+            category=category,
+            author=author,
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_steps='Recipe Preparation Steps',
+            preparation_steps_is_html=False,
+            is_published=True,
+        )
+        self.assertEqual(2, 1+1)
 
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
