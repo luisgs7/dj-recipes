1a6fc670a15e3f791b3887128c1663bed28bce86
luisgs7 <luisgsilva270@gmail.com>
2023-02-19 12:16:27 -0300

    new python package for tests

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -3,23 +3,6 @@ from django.urls import reverse, resolve
 from recipes import views
 
 
-class RecipeURLSTest(TestCase):
-    def test_recipe_home_url_is_correct(self):
-        url = reverse('recipes:home')
-
-        self.assertEqual(url, '/')
-
-    def test_recipe_category_url_is_correct(self):
-        url = reverse('recipes:category', kwargs={'category_id': 1})
-
-        self.assertEqual(url, '/recipes/category/1/')
-
-    def test_recipe_detail_recipe_url_is_correct(self):
-        url = reverse('recipes:recipe', kwargs={'id': 1})
-
-        self.assertEqual(url, '/recipes/1/')
-
-
 class RecipeViewsTest(TestCase):
     def test_reicpe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
