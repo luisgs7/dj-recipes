9e8c4053df1c439cf51c36cc7bad23f4db575a5d
luisgs7 <luisgsilva270@gmail.com>
2023-02-19 15:00:45 -0300

    new test: test_recipe_home_template_shows_no_recipes_found_if_no_recipes

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -23,3 +23,9 @@ class RecipeViewsTest(TestCase):
     def test_recipe_home_view_loads_correct_template(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn('<h1>No recipes found here :(</h1>',
+                      response.content.decode('utf-8'))
+        # A função decode é para converter de bytes para string
