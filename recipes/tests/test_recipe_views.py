7696c11628e730bd20257fbcbe2af4f0fdff056a
luisgs7 <luisgsilva270@gmail.com>
2023-02-21 16:26:58 -0300

    new test: test_recipe_category_template_dont_load_recipes_not_published, test_recipe_detail_template_dont_load_not_published

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -53,7 +53,6 @@ class RecipeViewsTest(RecipeTestBase):
             response.content.decode('utf-8')
         )
 
-
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
         self.assertIs(view.func, views.category)
@@ -79,6 +78,17 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertIn('10 Minutos', content)
         self.assertIn('5 Porções', content)
 
+    def test_recipe_category_template_dont_load_recipes_not_published(self):
+        """Test recipe is_published False dont show"""
+        # Need a recipe for this test
+        recipe = self.make_recipe(is_published=False)
+
+        response = self.client.get(reverse('recipes:recipe', kwargs={
+            'id': recipe.category.id
+        }))
+
+        self.assertEqual(response.status_code, 404)
+
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
@@ -103,3 +113,15 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertIn(needed_title, content)
         self.assertIn('10 Minutos', content)
         self.assertIn('5 Porções', content)
+
+    def test_recipe_detail_template_dont_load_not_published(self):
+        """Test recipe is_published False dont show"""
+        # Need a recipe for this test
+        recipe = self.make_recipe(is_published=False)
+
+        response = self.client.get(reverse(
+                'recipes:recipe',
+                kwargs={'id': recipe.id}
+        ))
+
+        self.assertEqual(response.status_code, 404)
