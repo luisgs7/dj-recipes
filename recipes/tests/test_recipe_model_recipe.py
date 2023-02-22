3c83413b2b3daa29d29341688b267a5cff7d1c10
luisgs7 <luisgsilva270@gmail.com>
2023-02-22 11:11:36 -0300

    new test: test_recipe_fields_max_lenght

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ValidationError
+from parameterized import parameterized
 
 from .test_recipe_base import RecipeTestBase
 
@@ -8,8 +9,19 @@ class RecipeModelTest(RecipeTestBase):
         self.recipe = self.make_recipe()
         return super().setUp()
 
-    def test_recipe_title_raises_error_if_title_has_more_than_65_chars(self):
-        self.recipe.title = 'A' * 70
+    # def test_recipe_title_raises_error_if_title_has_more_than_65_chars(self):
+    #     self.recipe.title = 'A' * 70
 
+    #     with self.assertRaises(ValidationError):
+    #         self.recipe.full_clean()
+
+    @parameterized.expand([
+        ('title', 65),
+        ('description', 165),
+        ('preparation_time_unit', 65),
+        ('servings_unit', 65),
+    ])
+    def test_recipe_fields_max_lenght(self, field, max_lenght):
+        setattr(self.recipe, field, 'A' * (max_lenght + 1))
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
