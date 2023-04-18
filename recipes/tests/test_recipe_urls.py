021c041d1d845df830b22d4dfd7d773eca79dfc9
luisgs7 <luisgsilva270@gmail.com>
2023-04-18 15:26:41 -0300

    new test: test_recipe_search_url_is_correct

--- recipes/tests/test_recipe_urls.py
+++ recipes/tests/test_recipe_urls.py
@@ -17,3 +17,9 @@ class RecipeURLSTest(TestCase):
         url = reverse('recipes:recipe', kwargs={'id': 1})
 
         self.assertEqual(url, '/recipes/1/')
+
+    def test_recipe_search_url_is_correct(self):
+        url = reverse('recipes:search')
+        self.assertEqual(url, '/recipes/search/')
+
+# RED - GREEN - REFACTOR
