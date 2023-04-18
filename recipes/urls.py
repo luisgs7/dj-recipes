e9d4c0d016fdc12ff01ed2ba3010433ecbe96076
luisgs7 <luisgsilva270@gmail.com>
2023-04-18 15:36:59 -0300

    format code

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,9 +4,9 @@ from recipes import views
 app_name = "recipes"
 
 urlpatterns = [
+    path("", views.home, name="home"),
+    path("recipes/search/", lambda request: ..., name="search"),
     path("recipes/category/<int:category_id>/", views.category,
          name="category"),
-    path("", views.home, name="home"),
     path("recipes/<int:id>/", views.recipe, name="recipe"),
-    path("recipes/search/", lambda request: ..., name="search"),
 ]
