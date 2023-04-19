0f453b89950947755d98a0b4d0636493be6415f6
luisgs7 <luisgsilva270@gmail.com>
2023-04-19 17:01:27 -0300

    new test: test_first_range_is_static_if_current_page_is_less_than_middle_page

--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -7,10 +7,28 @@ class PaginationTest(TestCase):
     def test_make_pagination_range_returns_a_pagination_range(self):
         pagination = make_pagination_range(
             page_range=list(range(1, 21)),
-            qtd_paginas=4,
+            qty_pages=4,
             current_page=1,
         )
         self.assertEqual([1, 2, 3, 4], pagination)
 
-    def test_first_range_is_static_if_current_page_is_less_than_middle_page(self):
-        ...
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=2,
+        )
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=3,
+        )
+        self.assertEqual([2, 3, 4, 5], pagination)
+
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=4,
+        )
+        self.assertEqual([3, 4, 5, 6], pagination)
