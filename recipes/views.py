7fa6ddb957c11c6d06c6d573a86c66641765005b
luisgs7 <luisgsilva270@gmail.com>
2023-04-19 21:38:38 -0300

    add pagination default

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.core.paginator import Paginator
 from django.http import Http404
 from django.shortcuts import render
 from recipes.models import Recipe
@@ -10,9 +11,13 @@ def home(request):
               is_published=True,
               ).order_by('-id')
 
+    current_page = request.GET.get('page', 1)
+    paginator = Paginator(recipes, 9)
+    page_obj = paginator.get_page(current_page)
+
     return render(request, "recipes/pages/home.html",
                   context={
-                      "recipes": recipes,
+                      "recipes": page_obj,
                   })
 
 
