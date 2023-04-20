66cd12fb2b19acfc0e38eaa70be21b4ca958286d
luisgs7 <luisgsilva270@gmail.com>
2023-04-19 21:18:09 -0300

    refactor tests

--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -9,26 +9,30 @@ class PaginationTest(TestCase):
             page_range=list(range(1, 21)),
             qty_pages=4,
             current_page=1,
-        )
+        )['pagination']
+
         self.assertEqual([1, 2, 3, 4], pagination)
 
         pagination = make_pagination_range(
             page_range=list(range(1, 21)),
             qty_pages=4,
             current_page=2,
-        )
+        )['pagination']
+
         self.assertEqual([1, 2, 3, 4], pagination)
 
         pagination = make_pagination_range(
             page_range=list(range(1, 21)),
             qty_pages=4,
             current_page=3,
-        )
+        )['pagination']
+
         self.assertEqual([2, 3, 4, 5], pagination)
 
         pagination = make_pagination_range(
             page_range=list(range(1, 21)),
             qty_pages=4,
             current_page=4,
-        )
+        )['pagination']
+
         self.assertEqual([3, 4, 5, 6], pagination)
