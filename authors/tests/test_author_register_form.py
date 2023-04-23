8096f0db0ba460ad996d42d8c6cfd0a39c03187f
luisgs7 <luisgsilva270@gmail.com>
2023-04-23 15:19:24 -0300

    new test: test_fields_label

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -32,3 +32,16 @@ class AuthorRegisterFormUnitTest(TestCase):
         form = RegisterForm()
         current_placeholder = form[field].field.help_text
         self.assertEqual(current_placeholder, needed)
+
+    @parameterized.expand([
+        ('username', 'Username'),
+        ('first_name', 'First name'),
+        ('last_name', 'Last name'),
+        ('email', 'E-mail'),
+        ('password', 'Password'),
+        ('password2', 'Password2'),
+    ])
+    def test_fields_label(self, field, needed):
+        form = RegisterForm()
+        current_placeholder = form[field].field.label
+        self.assertEqual(current_placeholder, needed)
