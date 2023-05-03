9c7d49401c57f9777c170f76bb7d9db61e6089d3
luisgs7 <luisgsilva270@gmail.com>
2023-05-03 10:56:05 -0300

    add login url

--- authors/urls.py
+++ authors/urls.py
@@ -6,6 +6,6 @@ app_name = 'authors'
 urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('register/create/', views.register_create, name='register_create'),
-    path('register/', views.login_view, name='login'),
+    path('login/', views.login_view, name='login'),
     path('login/create/', views.login_create, name='login_create'),
 ]
