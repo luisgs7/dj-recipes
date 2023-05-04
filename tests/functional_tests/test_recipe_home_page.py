eff4422871c751e88d7df9899816b691a9d6b923
luisgs7 <luisgsilva270@gmail.com>
2023-05-04 10:09:21 -0300

    add create recipe

--- tests/functional_tests/test_recipe_home_page.py
+++ tests/functional_tests/test_recipe_home_page.py
@@ -7,6 +7,7 @@ from .base import RecipeBaseFunctionalTest
 @pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
     def test_recipe_home_page_without_recipes_not_found_message(self):
+        # self.make_recipe()
         self.browser.get(self.live_server_url)
         # self.sleep(2)
         body = self.browser.find_element(By.TAG_NAME, 'body')
