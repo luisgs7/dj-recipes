006f471c22c3988a610f189abcbb8d20dff0d4a1
luisgs7 <luisgsilva270@gmail.com>
2023-05-04 11:11:15 -0300

    new test functional: test_recipe_search_input_can_find_correct_recipes

--- tests/functional_tests/test_recipe_home_page.py
+++ tests/functional_tests/test_recipe_home_page.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from unittest.mock import patch
 
 from .base import RecipeBaseFunctionalTest
@@ -7,10 +8,38 @@ from .base import RecipeBaseFunctionalTest
 
 @pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
-    @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_home_page_without_recipes_not_found_message(self):
         # self.make_recipe_in_batch()
         self.browser.get(self.live_server_url)
         # self.sleep(2)
         body = self.browser.find_element(By.TAG_NAME, 'body')
         self.assertIn('No recipes found here :(', body.text)
+
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_recipe_search_input_can_find_correct_recipes(self):
+        recipes = self.make_recipe_in_batch()
+
+        title_needed = 'This is what I need'
+        recipes[0].title = title_needed
+        recipes[0].save()
+
+        # Usuário abre a página
+        self.browser.get(self.live_server_url)
+
+        # Vê um campo de busca com o texto "Search for a recipe"
+        search_input = self.browser.find_element(
+            By.XPATH,
+            '//input[@placeholder="Search for a recipe"]'
+        )
+
+        # Clica neste input e digita o termo de busca
+        # para encontrar a receita o título desejado
+
+        search_input.send_keys(title_needed)
+        search_input.send_keys(Keys.ENTER)
+
+        # O usuário vê o que estava procurando na página
+        self.assertIn(
+           title_needed,
+           self.browser.find_element(By.CLASS_NAME, 'main-content-list').text,
+        )
