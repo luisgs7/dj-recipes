1d0f9342ed5b3c8c7ad4ea4bae295c67a71235ef
luisgs7 <luisgsilva270@gmail.com>
2023-05-03 21:17:38 -0300

    refactor functionals test

--- tests/functional_tests/test_recipe_home_page.py
+++ tests/functional_tests/test_recipe_home_page.py
@@ -1,21 +1,6 @@
-import time
-
-from django.test import LiveServerTestCase
 from selenium.webdriver.common.by import By
-from utils.browser import make_chrome_browser
 
-
-class RecipeBaseFunctionalTest(LiveServerTestCase):
-    def setUp(self) -> None:
-        self.browser = make_chrome_browser()
-        return super().setUp()
-
-    def tearDown(self) -> None:
-        self.browser.quit()
-        return super().tearDown()
-
-    def sleep(self, seconds=5):
-        time.sleep(seconds)
+from .base import RecipeBaseFunctionalTest
 
 
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
