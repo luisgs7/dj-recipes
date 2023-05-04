96a9ccdfc2eb733a6e7d55931d7b820f87d0ac29
luisgs7 <luisgsilva270@gmail.com>
2023-05-04 10:09:05 -0300

    add new heritage to RecipeBaseFunctionalTest, of RecipeMixin

--- tests/functional_tests/base.py
+++ tests/functional_tests/base.py
@@ -1,10 +1,12 @@
 import time
 
 from django.test import LiveServerTestCase
+
 from utils.browser import make_chrome_browser
+from recipes.tests.test_recipe_base import RecipeMixin
 
 
-class RecipeBaseFunctionalTest(LiveServerTestCase):
+class RecipeBaseFunctionalTest(LiveServerTestCase, RecipeMixin):
     def setUp(self) -> None:
         self.browser = make_chrome_browser()
         return super().setUp()
