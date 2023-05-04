edf3c510b23866560b8b9854a24117567dd52bb5
luisgs7 <luisgsilva270@gmail.com>
2023-05-04 10:36:40 -0300

    add @patch

--- tests/functional_tests/test_recipe_home_page.py
+++ tests/functional_tests/test_recipe_home_page.py
@@ -1,13 +1,15 @@
 import pytest
 from selenium.webdriver.common.by import By
+from unittest.mock import patch
 
 from .base import RecipeBaseFunctionalTest
 
 
 @pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
+    @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_home_page_without_recipes_not_found_message(self):
-        # self.make_recipe()
+        # self.make_recipe_in_batch()
         self.browser.get(self.live_server_url)
         # self.sleep(2)
         body = self.browser.find_element(By.TAG_NAME, 'body')
