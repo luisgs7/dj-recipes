e7f70a7a0470848c18253d400bb2ded31577dea4
luisgs7 <luisgsilva270@gmail.com>
2023-05-05 09:08:46 -0300

    new test functional: test_passwords_do_not_match

--- tests/functional_tests/authors/test_authors_register.py
+++ tests/functional_tests/authors/test_authors_register.py
@@ -79,3 +79,16 @@ class AuthorsRegister(AuthorsBaseTest):
 
             self.assertIn('The e-mail must be valid', form.text)
         self.form_field_test_with_callback(callback)
+
+    def test_passwords_do_not_match(self):
+        def callback(form):
+            password1 = self.get_by_placeholder(form, 'Type your password')
+            password2 = self.get_by_placeholder(form, 'Repeat your password')
+            password1.send_keys('P@ssw0rd')
+            password1.send_keys('P@ssw0rd_different')
+            password2.send_keys(Keys.ENTER)
+
+            form = self.get_form()
+
+            self.assertIn('Password and password2 must be equal', form.text)
+        self.form_field_test_with_callback(callback)
