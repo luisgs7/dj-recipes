500de50ec4b69fe92162a776a8483d23c0168380
luisgs7 <luisgsilva270@gmail.com>
2023-05-05 10:54:55 -0300

    new test functional: test_user_tries_to_logout_another_user

--- authors/tests/test_author_logout.py
+++ authors/tests/test_author_logout.py
@@ -17,3 +17,20 @@ class AuthorLogoutTest(TestCase):
             'Invalid logout request',
             response.content.decode('utf-8')
         )
+
+    def test_user_tries_to_logout_another_user(self):
+        User.objects.create_user(username='my_user', password='my_pass')
+        self.client.login(username='my_user', password='my_pass')
+
+        response = self.client.post(
+            reverse('authors:logout'),
+            data={
+                'username': 'another_user',
+            },
+            follow=True,
+        )
+
+        self.assertIn(
+            'Invalid logout user',
+            response.content.decode('utf-8')
+        )
