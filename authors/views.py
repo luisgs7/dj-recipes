31fca39a48e844e1cea3093f94d816fa72831303
luisgs7 <luisgsilva270@gmail.com>
2023-05-09 09:23:03 -0300

    refactor view dashboard

--- authors/views.py
+++ authors/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 from authors.forms import RegisterForm, LoginForm
+from recipes.models import Recipe
 
 
 def register_view(request):
@@ -87,4 +88,11 @@ def logout_view(request):
 
 @login_required(login_url='authors:login', redirect_field_name='next')
 def dashboard(request):
-    return render(request, 'authors/pages/dashboard.html')
+    recipes = Recipe.objects.filter(
+        is_published=False,
+        author=request.user,
+    )
+    return render(request, 'authors/pages/dashboard.html',
+                  context={
+                      'recipes': recipes,
+                  })
