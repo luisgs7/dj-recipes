4749f3a1b4135fadc45f401a1b4544f15fe4fe2c
luisgs7 <luisgsilva270@gmail.com>
2023-05-10 15:47:48 -0300

    update url id for pk

--- recipes/views/views_cbv.py
+++ recipes/views/views_cbv.py
@@ -1,6 +1,6 @@
 import os
 from django.http import Http404
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.db.models import Q
 
 from utils.pagination import make_pagination
@@ -97,3 +97,26 @@ class RecipeListViewSearch(RecipeListViewBase):
         })
 
         return ctx
+
+
+class RecipeDetail(DetailView):
+    model = Recipe
+    context_object_name = 'recipe'
+    template_name = 'recipes/pages/recipe-view.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+            is_published=True,
+        )
+
+        return qs
+
+    # def get_context_data(self, *args, **kwargs):
+    #     ctx = super().get_context_data(*args, **kwargs)
+
+    #     ctx.update({
+    #         'is_detail_page': True,
+    #     })
+
+    #     return ctx
