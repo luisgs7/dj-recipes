2518cdecbcc89559d36ef6712c8b6eceb6cac753
luisgs7 <luisgsilva270@gmail.com>
2023-05-12 11:13:28 -0300

    new view RecipeListViewTag

--- recipes/views/views_cbv.py
+++ recipes/views/views_cbv.py
@@ -7,6 +7,7 @@ from django.db.models import Q
 
 from utils.pagination import make_pagination
 from recipes.models import Recipe
+from tag.models import Tag
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 1))
 
@@ -23,6 +24,7 @@ class RecipeListViewBase(ListView):
             is_published=True,
         )
         qs = qs.select_related('author', 'category')
+        qs = qs.prefetch_related('tags')
 
         return qs
 
@@ -67,6 +69,36 @@ class RecipeListViewCategory(RecipeListViewBase):
             raise Http404()
 
         return qs
+
+
+class RecipeListViewTag(RecipeListViewBase):
+    template_name = 'recipes/pages/tag.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+           tags__slug=self.kwargs.get('slug', '')
+        )
+
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+
+        page_title = Tag.objects.filter(
+            slug=self.kwargs.get('slug', '')
+        ).first()
+
+        if not page_title:
+            page_title = 'No recipes found'
+
+        page_title = f'{page_title} - Tag |'
+
+        ctx.update({
+            'page_title': page_title,
+        })
+
+        return ctx
 
 
 class RecipeListViewSearch(RecipeListViewBase):
