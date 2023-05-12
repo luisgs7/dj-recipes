e2fb8eddd1bf1289811fdab34b7a39784398c6d0
luisgs7 <luisgsilva270@gmail.com>
2023-05-12 10:41:40 -0300

    config model recipe

--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,10 @@ from django.contrib.auth.models import User
 from django.utils.text import slugify
 from django.urls import reverse
 
+from django.contrib.contenttypes.fields import GenericRelation
+
+from tag.models import Tag
+
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
@@ -49,6 +53,7 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
+    tags = GenericRelation(Tag, related_query_name='recipes')
 
     def __str__(self) -> str:
         return self.title
