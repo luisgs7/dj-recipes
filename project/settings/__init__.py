ca5ec7951dc022b318b6e36aa324d8518d082a88
luisgs7 <luisgsilva270@gmail.com>
2023-05-12 21:31:24 -0300

    remove comments

--- project/settings/__init__.py
+++ project/settings/__init__.py
@@ -1,7 +1,7 @@
 # flake8: noqa
-from .environment import *  # isort:skip
-from .installed_apps import *  # isort:skip
-from .middlewares import *  # isort:skip
+from .environment import *
+from .installed_apps import *
+from .middlewares import *
 
 from .assets import *
 from .databases import *
@@ -10,4 +10,4 @@ from .messages import *
 from .security import *
 from .templates import *
 
-from .debug_toolbar import *  # isort:skip
+from .debug_toolbar import *
