32055925de754bd0a9f2de21e8b0c8718368f177
luisgs7 <luisgsilva270@gmail.com>
2023-05-15 16:24:10 -0300

    update tests

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -15,11 +15,11 @@ class RecipeModelTest(RecipeTestBase):
     #     with self.assertRaises(ValidationError):
     #         self.recipe.full_clean()
 
-    def make_recipe_no_defaults(self):
+    def make_recipe_no_defaults(self, title='Recipe Title'):
         recipe = Recipe(
             category=self.make_category(name='Test Default Category'),
             author=self.make_author(username='newuser'),
-            title='Recipe Title',
+            title=title,
             description='Recipe Description',
             slug='recipe-slug-for-no-defaults',
             preparation_time=10,
@@ -44,14 +44,14 @@ class RecipeModelTest(RecipeTestBase):
             self.recipe.full_clean()
 
     def test_recipe_preparation_steps_is_html_is_false_by_default(self):
-        recipe = self.make_recipe_no_defaults()
+        recipe = self.make_recipe_no_defaults(title='Recipe test is html')
         self.assertFalse(
             recipe.preparation_steps_is_html,
             msg='Recipe preparation_steps_is_html is not False',
         )
 
     def test_recipe_is_published_is_false_by_default(self):
-        recipe = self.make_recipe_no_defaults()
+        recipe = self.make_recipe_no_defaults(title='Recipe test is published')
         self.assertFalse(
             recipe.is_published,
             msg='Recipe is_published is not False',
