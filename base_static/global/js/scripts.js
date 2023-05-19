e4fe536e775ccb993a7d5f2c782a3f6a30a0a9d2
luisgs7 <luisgsilva270@gmail.com>
2023-05-19 15:31:37 -0300

    add animation for menu

--- base_static/global/js/scripts.js
+++ base_static/global/js/scripts.js
@@ -14,4 +14,34 @@ function my_scope() {
     }
 }
 
+const menuStyle = () => {
+    const buttonCloseMenu = document.querySelector('.button-close-menu')
+    const buttonShowMenu = document.querySelector('.button-show-menu')
+    const menuContainer = document.querySelector('.menu-container')
+
+    const buttonShowMenuVisibleClass = 'button-show-menu-visible'
+    const menuHiddenClass = 'menu-hidden';
+
+    const closeMenu = () => {
+        buttonShowMenu.classList.add(buttonShowMenuVisibleClass)
+        menuContainer.classList.add(menuHiddenClass)
+    }
+
+    const showMenu = () => {
+        buttonShowMenu.classList.remove(buttonShowMenuVisibleClass)
+        menuContainer.classList.remove(menuHiddenClass)
+    }
+
+    if (buttonCloseMenu) {
+        buttonCloseMenu.removeEventListener('click', closeMenu)
+        buttonCloseMenu.addEventListener('click', closeMenu)
+    }
+
+    if (buttonShowMenu) {
+        buttonShowMenu.removeEventListener('click', showMenu)
+        buttonShowMenu.addEventListener('click', showMenu)
+    }
+}
+
+menuStyle()
 my_scope()
