096a3a09faeca5a73089ea14cba8b9f27bdc4e93
luisgs7 <luisgsilva270@gmail.com>
2023-05-19 15:50:48 -0300

    config logout

--- base_static/global/js/scripts.js
+++ base_static/global/js/scripts.js
@@ -43,5 +43,18 @@ const menuStyle = () => {
     }
 }
 
+const userLogout = () => {
+    const authorsLogoutLinks = document.querySelectorAll('.authors-logout-link')
+    const formLogout = document.querySelector('.form-logout')
+
+    for (const link of authorsLogoutLinks) {
+        link.addEventListener('click', (e) => {
+            e.preventDefault()
+            formLogout.submit()
+        })
+    }
+}
+
+userLogout()
 menuStyle()
 my_scope()
