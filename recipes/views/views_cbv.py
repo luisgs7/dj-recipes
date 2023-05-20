60af5aadd04bae2225d1440afb421d1f1ea6b32a
luisgs7 <luisgsilva270@gmail.com>
2023-05-20 11:26:40 -0300

    new category translate

--- recipes/views/views_cbv.py
+++ recipes/views/views_cbv.py
@@ -4,6 +4,8 @@ from django.http import JsonResponse
 from django.http import Http404
 from django.views.generic import ListView, DetailView
 from django.db.models import Q
+from django.utils import translation
+from django.utils.translation import gettext as _
 
 from utils.pagination import make_pagination
 from recipes.models import Recipe
@@ -37,8 +39,14 @@ class RecipeListViewBase(ListView):
             PER_PAGE,
         )
 
+        html_language = translation.get_language()
+
         ctx.update(
-            {'recipes': page_obj, 'pagination_range': pagination_range}
+            {
+             'recipes': page_obj,
+             'pagination_range': pagination_range,
+             'html_language': html_language,
+             }
         )
         return ctx
 
@@ -53,8 +61,11 @@ class RecipeListViewCategory(RecipeListViewBase):
     def get_context_data(self, *args, **kwargs):
         ctx = super().get_context_data(*args, **kwargs)
 
+        category_translation = _('Category')
+
         ctx.update({
-            'title': f'{ctx.get("recipes")[0].category.name} - Category | ',
+            'title': f'{ctx.get("recipes")[0].category.name} \
+                - {category_translation} ',
         })
 
         return ctx
