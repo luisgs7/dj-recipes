40fb37c7297bf2a36cd8479cc5b5820c25ea255c
luisgs7 <luisgsilva270@gmail.com>
2023-05-20 14:26:08 -0300

    new signal recipe_cover_update

--- recipes/signals.py
+++ recipes/signals.py
@@ -1,6 +1,6 @@
 import os
 
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
 
 from recipes.models import Recipe
@@ -15,5 +15,18 @@ def delete_cover(instance):
 
 @receiver(pre_delete, sender=Recipe)
 def recipe_cover_delete(sender, instance, *args, **kwargs):
-    old_recipe = Recipe.objects.get(pk=instance.pk)
+    old_recipe = Recipe.objects.filter(pk=instance.pk).first()
     delete_cover(old_recipe)
+
+
+@receiver(pre_save, sender=Recipe)
+def recipe_cover_update(sender, instance, *args, **kwargs):
+    old_instance = Recipe.objects.filter(pk=instance.pk).first()
+
+    if not old_instance:
+        return
+
+    is_new_cover = old_instance.cover != instance.cover
+
+    if is_new_cover:
+        delete_cover(old_instance)
