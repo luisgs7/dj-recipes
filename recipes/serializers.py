f79f4e687ba826505599f7d1107ea81696d4f5eb
luisgs7 <luisgsilva270@gmail.com>
2023-05-22 11:02:33 -0300

    new field tag_links

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -30,6 +30,12 @@ class RecipeSerializer(serializers.Serializer):
         many=True,
         source='tags',
     )
+    tag_links = serializers.HyperlinkedRelatedField(
+        many=True,
+        source='tags',
+        queryset=Tag.objects.all(),
+        view_name='recipes:recipes_api_v2_tag',
+    )
 
     def any_method_name(self, recipe):
         return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
