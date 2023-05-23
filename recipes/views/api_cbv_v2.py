f58c32d1e10f8e617bdf3a428081edf93e76002b
luisgs7 <luisgsilva270@gmail.com>
2023-05-23 16:45:55 -0300

    new method create

--- recipes/views/api_cbv_v2.py
+++ recipes/views/api_cbv_v2.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import PageNumberPagination
+from rest_framework import status
 
 from recipes.models import Recipe
 from recipes.permissions import IsOwner
@@ -62,6 +63,17 @@ class RecipeAPIv2ViewSet(ModelViewSet):
         if self.request.method in ['PATCH', 'DELETE']:
             return [IsOwner(), ]
         return super().get_permissions()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=request.user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers=headers,
+        )
 
     def partial_update(self, request, *args, **kwargs):
         recipe = self.get_object()
