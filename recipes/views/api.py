b40d09af82154fa205311f4feab9b88dd01a1003
luisgs7 <luisgsilva270@gmail.com>
2023-05-22 21:29:42 -0300

    CRUD recipes ok

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -47,7 +47,7 @@ def recipe_api_list(request):
         #     )
 
 
-@api_view()
+@api_view(http_method_names=['GET', 'PATCH', 'DELETE'])
 def recipe_api_detail(request, pk):
     recipe = get_object_or_404(
         Recipe.objects.get_published(),
@@ -59,7 +59,33 @@ def recipe_api_detail(request, pk):
         context={
             'request': request,
         })
-    return Response(serializer.data)
+
+    if request.method == 'GET':
+        serializer = RecipeSerializer(
+            instance=recipe,
+            many=False,
+            context={'request': request},
+        )
+        return Response(serializer.data)
+
+    if request.method == 'PATCH':
+        serializer = RecipeSerializer(
+            instance=recipe,
+            data=request.data,
+            many=False,
+            context={'request': request},
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            serializer.data,
+        )
+
+    elif request.method == 'DELETE':
+        recipe.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
     # recipe = Recipe.objects.get_published().filter(pk=pk).first()
 
     # if recipe:
