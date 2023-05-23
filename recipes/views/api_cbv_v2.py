fd8391ac916a487cbaab48f8d48e560235f88910
luisgs7 <luisgsilva270@gmail.com>
2023-05-23 10:34:13 -0300

    new class RecipeAPIv2ViewSet

--- recipes/views/api_cbv_v2.py
+++ recipes/views/api_cbv_v2.py
@@ -3,6 +3,7 @@ from rest_framework.generics import (
     RetrieveUpdateDestroyAPIView,
     )
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import PageNumberPagination
 
 from recipes.models import Recipe
@@ -13,6 +14,28 @@ from recipes.serializers import (
 
 class RecipeAPIv2Pagination(PageNumberPagination):
     page_size = 3
+
+
+class RecipeAPIv2ViewSet(ModelViewSet):
+    queryset = Recipe.objects.get_published()
+    serializer_class = RecipeSerializer
+    pagination_class = RecipeAPIv2Pagination
+
+    def patch(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        recipe = self.get_queryset().filter(pk=pk).first()
+        serializer = RecipeSerializer(
+            instance=recipe,
+            data=request.data,
+            many=False,
+            context={'request': request},
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            serializer.data,
+        )
 
 
 class RecipeAPIv2List(ListCreateAPIView):
